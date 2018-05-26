2a8b5d8d13d3955fe255bdd949ead1d4a258d329
thakursachin467 <thakursachin467@gmail.com>
2018-05-27 03:48:40 +0530

    added product images

--- routes/admin_products.js
+++ routes/admin_products.js
@@ -42,7 +42,42 @@ router.get('/add-product',(req,res)=>{
 });
 
 router.post('/add-product',(req,res)=>{
-      console.log(req.body);
+      let title= req.body.title;
+      let slug= req.body.title.replace(/\s+/g,'-').toLowerCase();
+      let price= req.body.price;
+      let pricenew= parseFloat(price).toFixed(2);
+      let description= req.body.description;
+      let category= req.body.category;
+      let image= req.files.image.name;
+      products.findOne({slug:slug})
+      .then((product)=>{
+          if(product) {
+            req.flash('error_msg','This Product Already Exists');
+            res.redirect('/admin/products/add-product');
+          } else {
+              let productnew= new products({
+                title:title,
+                slug:slug,
+                price:pricenew,
+                description:description,
+                category:category,
+                image:image
+              });
+              productnew.save()
+              .then(()=>{
+                  mkdirp('public/product_images/'+ productnew._id,error => console.log(error) );
+                  mkdirp('public/product_images/'+ productnew._id+'/gallery',error => console.log(error) );
+                  mkdirp('public/product_images/'+ productnew._id+'/gallery/thumbs',error => console.log(error) );
+                  let productimage= req.files.image;
+                    console.log(productimage.mv());
+                  let path= 'public/product_images/'+ productnew._id +'/'+image;
+                  productimage.mv(path,error => console.log(error));
+                  req.flash('success_msg','Product Added Sucessfully');
+                  res.redirect('/admin/products');
+              });
+          }
+      });
+
 });
 
 
